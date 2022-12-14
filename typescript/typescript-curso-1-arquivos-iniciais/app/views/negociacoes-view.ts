392fac51bc1ff7171228e7f2db9259581eead409
Camila Zucchi <camilazucchi@outlook.com>
2022-12-14 14:31:52 -0300

    feat: adding negociacoes view

---
+++ typescript/typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
@@ -0,0 +1,18 @@
+export class NegociacoesView {
+  template(): string {
+    // retorna uma string html + dados fundidos no html
+    return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        `;
+  }
+}
